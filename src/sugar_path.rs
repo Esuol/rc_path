be6aaee0ab081e1df29524cf9bd8ae4967aa6ded
berlinen <1292393108@qq.com>
2024-03-28 14:09:58 +0800

    feat: impl SugarPath::normalize

--- src/sugar_path.rs
+++ src/sugar_path.rs
@@ -12,4 +12,23 @@ pub trait SugarPath {
     fn normalize(&self) -> Cow<Path>;
 
     fn absolutize(&self) -> Cow<Path>;
+
+    fn relative(&self, to: impl AsRef<Path>) -> PathBuf;
+}
+
+impl SugarPath for Path {
+    fn normalize(&self) -> Cow<Path> {
+        let mut components = normalize_to_component_vec(self);
+        if components.is_empty() {
+            return Cow::Borrowed(Path::new("."));
+        }
+
+        if cfg!(target_family = "windows") {
+            if components.len() == 1 && matches!(components[0], Component::Prefix(_)) {
+                components.push(Component::CurDir);
+            }
+        }
+
+        component_vec_to_path_buf(components).into()
+    }
 }
