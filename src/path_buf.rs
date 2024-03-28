3e80203e718cd5ff627f2ba234a7a03e43919106
berlinen <1292393108@qq.com>
2024-03-28 10:57:23 +0800

    feat: define pathBuf

--- src/path_buf.rs
+++ src/path_buf.rs
@@ -1,1 +1,9 @@
 use std::path::{Component, PathBuf};
+
+use crate::utils::{component_vec_to_path_buf, normalize_to_component_vec, CWD};
+
+pub trait PathBuf {
+    fn into_normalize(self) -> PathBuf;
+
+    fn into_absolutize(self) -> PathBuf;
+}
