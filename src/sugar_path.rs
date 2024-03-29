43b2af268fe29157af7e04f3d1097c3b5ec3b095
berlinen <1292393108@qq.com>
2024-03-29 10:50:14 +0800

    feat: path::relative

--- src/sugar_path.rs
+++ src/sugar_path.rs
@@ -56,4 +56,76 @@ impl SugarPath for Path {
             cwd.into_normalize().into()
         }
     }
+
+    fn relative(&self, to: impl AsRef<Path>) -> PathBuf {
+        let base = to.as_ref().absolutize();
+        let target = self.absolutize();
+        let mut ret = PathBuf::new();
+        if base == target {
+            return PathBuf::new();
+        } else {
+            let base_components = base
+                .components()
+                .into_iter()
+                .filter(|com| {
+                    matches!(
+                        com,
+                        Component::Normal(_) | Component::Prefix(_) | Component::RootDir
+                    )
+                })
+                .collect::<Vec<_>>();
+            let target_components = target
+                .components()
+                .into_iter()
+                .filter(|com| {
+                    matches!(
+                        com,
+                        Component::Normal(_) | Component::Prefix(_) | Component::RootDir
+                    )
+                })
+                .collect::<Vec<_>>();
+
+            let longest_len = if base_components.len() > target_components.len() {
+                base_components.len()
+            } else {
+                target_components.len()
+            };
+            let mut i = 0;
+            while i < longest_len {
+                let from_component = base_components.get(i);
+                let to_component = target_components.get(i);
+
+                if cfg!(target_family = "windows") {
+                    if let Some(Component::Normal(from_seg)) = from_component {
+                        if let Some(Component::Normal(to_seg)) = to_component {
+                            if from_seg.to_ascii_lowercase() == to_seg.to_ascii_lowercase() {
+                                i += 1;
+                                continue;
+                            }
+                        }
+                    }
+                }
+
+                if from_component != to_component {
+                    break;
+                }
+
+                i += 1;
+            }
+
+            let mut from_start = i;
+            while from_start < base_components.len() {
+                ret.push("..");
+                from_start += 1;
+            }
+
+            let mut to_start = i;
+            while to_start < target_components.len() {
+                ret.push(target_components[to_start]);
+                to_start += 1;
+            }
+
+            ret
+        }
+    }
 }
